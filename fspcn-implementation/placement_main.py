2738c52103097c159ee74f103de0ce699698a481
Hafid Nur <hafidnurazis@gmail.com>
2025-05-29 02:50:13 +0700

    placement with CN optimization
    - add CN optimization from referenced paper
    - update placement main function

--- fspcn-implementation/placement_main.py
+++ fspcn-implementation/placement_main.py
@@ -1,6 +1,7 @@
 import random
 import experiment_configuration
 import my_config
+import CN_optimization
 
 #
 # Initialization and set up
@@ -9,6 +10,8 @@ random.seed(8)
 verbose_log = False
 generate_plots = True
 
+CommunityOptimization = True
+
 config = my_config.MyConfig()
 ec = experiment_configuration.ExperimentConfiguration(config)
 ec.load_configuration(config.my_configuration)
@@ -16,3 +19,11 @@ ec.load_configuration(config.my_configuration)
 ec.network_generation()
 ec.app_generation()
 ec.user_generation()
+
+#
+# Graph Partition Optimization
+#
+
+if CommunityOptimization:
+    cno_ = CN_optimization.CNoptimization(ec, config)
+    service_to_device_placement_matrix = cno_.solve()
