e84f866ece502edbb8c837b71a0b4fb6c5798d6a
Hafid Nur <73023445+hafidnrzs@users.noreply.github.com>
2025-07-21 06:42:47 +0700

    jumlah aplikasi = 40

--- experiments/PartitionILPPlacement/spaguetti.py
+++ experiments/PartitionILPPlacement/spaguetti.py
@@ -261,7 +261,7 @@ if myConfiguration_ == "fspcn":
     func_NODESPEED = "random.randint(100,1000)"  # INTS / MS #random distribution for the speed of the fog devices
 
     # APP and SERVICES
-    TOTALNUMBEROFAPPS = 20
+    TOTALNUMBEROFAPPS = 40
     func_APPGENERATION = "nx.gn_graph(random.randint(2,10))"  # algorithm for the generation of the random applications
     func_SERVICEINSTR = "random.randint(20000,60000)"  # INSTR --> teniedno en cuenta nodespped esto nos da entre 200 y 600 MS
     func_SERVICEMESSAGESIZE = "random.randint(1500000,4500000)"  # BYTES y teniendo en cuenta net bandwidth nos da entre 20 y 60 MS
